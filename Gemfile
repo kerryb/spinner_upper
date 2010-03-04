478eb810bd3b1ed87011c083bb4a26f60912e063
Kerry Buckley <kerryjbuckley@gmail.com>
2010-03-04 14:03:31 +0000

    First stab at talking to EC2

--- Gemfile
+++ Gemfile
@@ -7,8 +7,8 @@ disable_system_gems
 bundle_path 'gems'
 
 gem 'rails', '2.3.5'
-
 gem 'postgres-pr', '0.6.3'
+gem 'kerryb-right_aws', '1.10.2'
 
 except :production do
   gem 'rspec', '1.3.0'
--- app/models/instance.rb
+++ app/models/instance.rb
@@ -1,4 +1,11 @@
 class Instance
   def self.create
+    ENV['EC2_URL'] = "http://c2.harmo.cloud21cn.com:8773/services/Eucalyptus"
+    ec2 = RightAws::Ec2.new ACCESS_KEY, SECRET_KEY, :proxy => ENV['http_proxy']
+    image_id = "emi-4EEF19DC"
+    group_name = "default"
+    key_name = "team7"
+    response = ec2.run_instances image_id, 1, 1, [group_name], key_name
+    p response
   end
 end
