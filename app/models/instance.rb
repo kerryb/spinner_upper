e3eab27de95679636ce1c47b61a33c50831ba6e0
Kerry Buckley <kerryjbuckley@gmail.com>
2010-03-04 14:29:19 +0000

    Pull out EC2 URL into config

--- app/models/instance.rb
+++ app/models/instance.rb
@@ -1,11 +1,13 @@
+require 'right_aws'
+
 class Instance
   def self.create
-    ENV['EC2_URL'] = "http://c2.harmo.cloud21cn.com:8773/services/Eucalyptus"
+    ENV['EC2_URL'] = EC2_URL
     ec2 = RightAws::Ec2.new ACCESS_KEY, SECRET_KEY, :proxy => ENV['http_proxy']
     image_id = "emi-4EEF19DC"
     group_name = "default"
     key_name = "team7"
     response = ec2.run_instances image_id, 1, 1, [group_name], key_name
-    p response
+    RAILS_DEFAULT_LOGGER.debog response.inspect
   end
 end
