a9f0dece3021750d0be561bc503d1cf6c5c0917d
Kerry Buckley <kerryjbuckley@gmail.com>
2010-03-04 13:25:53 +0000

    Restart on deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -31,4 +31,5 @@ end
 task :after_update_code do
   run "ln -nfs #{shared_path}/production.rb #{release_path}/config/environments/production.rb"
   run "cd #{release_path} && ./script/bundle"
+  touch "#{release_path}/tmp/restart.txt"
 end
